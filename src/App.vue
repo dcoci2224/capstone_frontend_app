922954ce8b5510bb707c7a92eb14c79b260577a2
Dan Coci <dcoci2224@gmail.com>
2020-05-11 21:31:30 -0500

    started carted_products index/ shoppring cart

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,8 @@
       <router-link to="/login">Login</router-link>
       |
       <router-link to="/logout">Logout</router-link>
+      |
+      <router-link to="/carted_products">Shopping Cart</router-link>
     </div>
     <router-view />
   </div>
---
+++ src/views/CartedProductIndex.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="container">
+    <h1>Shopping Cart</h1>
+    <div v-for="carted_product in carted_products">
+      <h2>{{ country.name }}</h2>
+      <p>{{ country.nickname }}</p>
+      <p>World Ranking {{ country.world_ranking }}</p>
+      <div>
+        <router-link to="/countries/">Back to all countries</router-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style></style>
+
+<script>
+import axios from "axios";
+export default {
+  data: function() {
+    return {
+      carted_products: [],
+    };
+  },
+  created: function() {
+    axios.get("/api/carted_products").then(response => {
+      console.log(response);
+      this.carted_products = response.data;
+    });
+  },
+  methods: {
+    indexCartedProducts: function() {
+      axios.get("/api/carted_products").then(response => {
+        console.log("Get all carted_products: ", response);
+        this.carted_products = response.data;
+      });
+    },
+  },
+};
+</script>
